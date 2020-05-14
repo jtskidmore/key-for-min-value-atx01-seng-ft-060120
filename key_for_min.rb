ac0b1810d1110384370c4b80f957b186a409d384
JT Skidmore <johntoddskidmore@gmail.com>
2020-05-14 22:14:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  ikea_prices = []
 
+  name_hash.each do |key, value|
+    ikea_prices.push(value)
+  end
+
+  ikea_prices.sort!
+  
+  ikea_prices[0]
+  
 end
